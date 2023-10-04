f207e6c0f1afec38902b248f7da3b4902eef5fdc
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-10-04 09:01:35 -0300

    add: Exercicio 09

--- Tests/UmADez/Tests09.cs
+++ Tests/UmADez/Tests09.cs
@@ -12,10 +12,12 @@ namespace Tests.UmADez
         [InlineData("JOAO", new double[] { 500.00, 1230.30 }, "TOTAL = R$ 684,54")]
         [InlineData("PEDRO", new double[] { 700.00, 0.00 }, "TOTAL = R$ 700,00")]
         [InlineData("MANGOJATA", new double[] { 1700.00, 1230.50 }, "TOTAL = R$ 1.884,58")]
-        public void Sucesso(double[] nums, string resultadoEsperado)
+        public void Sucesso(string funcionario,double[] nums, string resultadoEsperado)
         {
+
             double calculo = (nums[1] * 0.15) + nums[0];
             string resultado = $"TOTAL = {calculo:C2}";
+            Assert.True(!string.IsNullOrEmpty(funcionario));
             Assert.Equal(resultadoEsperado, resultado);
         }
     }
