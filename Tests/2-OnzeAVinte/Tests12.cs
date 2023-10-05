9e299b54532b3ea5639f74d01556f5f9c62c7dc7
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-10-05 10:22:20 -0300

    add: Exercicio 12

--- Tests/2-OnzeAVinte/Tests12.cs
+++ Tests/2-OnzeAVinte/Tests12.cs
@@ -18,7 +18,18 @@ namespace Tests.OnzeAVinte
         })]
         public void Test1(double[] num, string[] resultados)
         {
-            Assert.True(true);
+            double calculoTriangulo = num[0] * num[2] / 2;
+            double calculoCirculo = 3.14159 * Math.Pow(num[2], 2);
+            double calculoTrapezio = ((num[0] + num[1]) * num[2]) / 2;
+            double calculoQuadrado = Math.Pow(num[1], 2);
+            double calculoRetangulo = num[0] * num[1];
+
+            Assert.Equal(resultados[0], $"TRIANGULO: {calculoTriangulo:F3}");
+            Assert.Equal(resultados[1], $"CIRCULO: {calculoCirculo:F3}");
+            Assert.Equal(resultados[2], $"TRAPEZIO: {calculoTrapezio:F3}");
+            Assert.Equal(resultados[3], $"QUADRADO: {calculoQuadrado:F3}");
+            Assert.Equal(resultados[4], $"RETANGULO: {calculoRetangulo:F3}");
+
         }
     }
 }
